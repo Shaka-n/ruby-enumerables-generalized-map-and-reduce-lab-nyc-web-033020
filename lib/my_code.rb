dea8f85d681497f3af3ac6f848cfefcc1485a828
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-28 19:12:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,9 @@ def map(source_array)
   index = 0 
   new_array = []
   while index < 0 do
+    puts source_array[index]
     yield(source_array[index])
+    puts source_array[index]
     index += 1
   end
 end
