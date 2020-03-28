fb66e5990cd1b2e3534b56ea4b26e326032a4ffe
Garen Sahagian <g.s.sahagian@gmail.com>
2020-03-28 19:16:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,6 +14,7 @@ def reduce(source_array, starting_point = 0)
   result = starting_point
   while index < source_array.length do 
     result = yield(source_array[index])
+    index += 1 
   end
   result
 end
